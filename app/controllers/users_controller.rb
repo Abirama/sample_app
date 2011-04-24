0752706109179d676e57ca56c58d8a7c97c10bd0
Abirama <simonng47@rogers.com>
2011-04-23 22:49:23 -0400

    Added user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,11 +67,13 @@ class UsersController < ApplicationController
 	end
   end
   
+  def show
+  	@user = User.find(params[:id])
+  	@microposts = @user.microposts.paginate(:page => params[:page])
+  	@title = @user.name
+  end
+  
   private
-  	
-  	def authenticate
-  		deny_access unless signed_in?
-  	end
   	
   	def correct_user
   		@user = User.find(params[:id])
@@ -81,5 +83,6 @@ class UsersController < ApplicationController
   	def admin_user
   		redirect_to(root_path) unless current_user.admin?
   	end
+  
   
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -35,6 +35,15 @@ module SessionsHelper
 		redirect_to(session[:return_to] || default)
 		clear_return_to
 	end
+
+	def authenticate
+		deny_access unless signed_in?
+	end
+	
+	def deny_access
+		store_location
+		redirect_to signin_path, :notice => "Please sign in to access this page."
+	end
 	
 	private
 	
@@ -53,6 +62,5 @@ module SessionsHelper
 		def clear_return_to
 			session[:return_to] = nil
 		end
-
 
 end
